9998df62669c153768d3be824b277b315c9d5e49
Ubada <ubadaaleem14@gmail.com>
2026-06-22 16:41:00 +0500

    Fixed syntax

--- main.py
+++ main.py
@@ -2,50 +2,39 @@ from tkinter import *
 
 def calculate_fee():
     if hours.get() == "2":
-        fee.set("Rs. 18000")
-
+        fee.set("Rs. 18,000")
     elif hours.get() == "3":
-        fee.set("Rs. 53000")     
-
+        fee.set("Rs. 51,500")
     elif hours.get() == "4":
-        fee.set("Rs. 75000")
+        fee.set("Rs. 75,000")
 
 root = Tk()
-root.title("Fee Calculator")
-root.geometry("350x300") 
+root.title("Student Fee Calculator")
+root.geometry("350x300")
 
-Label(root, text= "Fee Calculator"
-      font= ("Aerial", 16, "bold")).pack(pady=10)
+Label(root, text="Student Fee Calculator",
+      font=("Arial", 16, "bold")).pack(pady=10)
 
 gender = StringVar()
 gender.set("Male")
 
-Label(root, text="Select gender").pack()
-
-Radiobutton(root,
-            text ="Male",
-            variable=gender,
-            value="Male").pack()
-
-Radiobutton(root,
-            text="Female",
-            variable=gender,
-            value="Female").pack()
+Label(root, text="Select Gender").pack()
+Radiobutton(root, text="Male",
+            variable=gender, value="Male").pack()
+Radiobutton(root, text="Female",
+            variable=gender, value="Female").pack()
 
 hours = StringVar()
 hours.set("2")
 
-Label(root, text="Credit hours").pack(pady=10)
+Label(root, text="Credit Hours").pack(pady=10)
 OptionMenu(root, hours, "2", "3", "4").pack()
-
 
 Button(root, text="Calculate Fee",
        command=calculate_fee).pack(pady=10)
 
 fee = StringVar()
-Label(root, textvariable==fee,
+Label(root, textvariable=fee,
       font=("Arial", 12, "bold")).pack()
 
 root.mainloop()
-
-
